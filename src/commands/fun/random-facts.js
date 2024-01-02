ad686400de2003d0aacbb70de920c70bc310c08c
Nicholas <contactnicholas81@gmail.com>
2024-01-02 22:29:08 +0000

    first commit

---
+++ src/commands/fun/random-facts.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+
+require('dotenv').config();
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('facts')
+		.setDescription('Replies with tons of random interesting facts about cool topics.'),
+	async execute(interaction) {
+        const url = 'https://api.api-ninjas.com/v1/facts';
+
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    'X-Api-Key': process.env.API_NINJAS_API_KEY,
+                },
+            });
+
+            // Sample Response
+            // [
+            //     {
+            //       "fact": ""
+            //     }
+            // ]
+
+            const fact = response.data[0].fact;
+
+            await interaction.reply(fact);
+        } catch (error) {
+            await interaction.reply('Something went wrong with the request. Please try again later.');
+            console.error('Error:', error.message);
+        }
+	},
+};
