c559638a44c686b289d5dac9274e7e62ea99c77e
Nicholas <contactnicholas81@gmail.com>
2024-06-07 17:02:39 +0100

    something

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,11 +1,15 @@
 const AVATAR = 'old/nicholas-logo-old.png';
 const BOT_NAME = 'Nicholas Bot';
+const FIRST_NAME = 'Nicholas';
+const LAST_NAME = 'Njoki';
 const DOMAIN = 'notnick.io';
 const THEME = '#F54BFF';
 
 module.exports = {
     AVATAR,
     BOT_NAME,
+    FIRST_NAME,
+    LAST_NAME,
     DOMAIN,
     THEME,
 };
